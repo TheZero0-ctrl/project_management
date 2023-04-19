38045e456d7f40f24e6b290d2735bb86658a6117
TheZero0-ctrl <ankitpariyar486@gmail.com>
2023-04-19 23:00:09 +0545

    generate projects_controller, home_controler and add some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :projects
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
-  root 'projects#index'
+
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'primary': '#FF9F00',
+        'primary-dark': '#0B6643',
+        'secondary': '#00B8B8',
+        'accent1': '#FF6B6B',
+        'accent2': '#4D4D4D',
+        'accent3': '#B2FF59',
+      },
     },
   },
   plugins: [
